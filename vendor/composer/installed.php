b7cca8a74e87e92d1f887822855b34b028039727
Emleonstz <emleons23@gmail.com>
2023-06-24 02:03:18 +0300

    updated

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,20 +1,20 @@
 <?php return array(
     'root' => array(
         'name' => 'emleons/tz_namba',
-        'pretty_version' => '1.0.0',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
-        'type' => 'library',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '06dac91cc4cb4e005f4b1137fb09c1dbe74a5446',
+        'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
         'emleons/tz_namba' => array(
-            'pretty_version' => '1.0.0',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
-            'type' => 'library',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '06dac91cc4cb4e005f4b1137fb09c1dbe74a5446',
+            'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
